679a4a3b8473997e0ac09bfc2af073c4aa8ae0a3
Yusuf <yusuf_zeki1234@hotmail.com>
2023-04-05 13:52:03 +0300

    NewMethods

---
+++ Kod ders/MementoDesign/MementoDesign/Program.cs
@@ -0,0 +1,101 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace MementoDesign
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book = new Book
+            {
+                İsbn = "123",
+                Title = "Yusuf",
+                Author = " Arslan "
+            };
+            book.ShowBook();
+            CareTaker careTaker = new CareTaker();
+            careTaker.Memento = book.Createundo();
+
+            book.İsbn = "321";
+            book.Title = "Fusuy";
+
+            book.ShowBook();
+
+            book.RestoreFromUndo(careTaker.Memento);
+            book.ShowBook();
+
+            Console.ReadLine();
+        }
+    }
+    class Book
+    {
+        private string _Title { get; set; }
+        private string _Author { get; set; }
+        private string _isbn { get; set; }
+        private DateTime _lastEdaited;
+        public string Title
+        {
+            get { return _Title; }
+            set 
+            {
+                _Title = value;
+                SetLastEdited();
+            }
+        }
+        public string Author
+        {
+            get { return _Author; }
+            set
+            { 
+                _Author = value;
+                SetLastEdited();
+            }
+        }
+        public string İsbn
+        {
+            get { return _isbn; }
+            set
+            {
+                _isbn = value;
+                SetLastEdited();
+            }
+        }
+        private void SetLastEdited()
+        {
+            _lastEdaited=DateTime.UtcNow;
+        }
+        public Memento Createundo()
+        {
+            return new Memento(_isbn,_Title,_Author,_lastEdaited);
+        }
+        public void RestoreFromUndo(Memento memento)
+        {
+            _Title=memento.Title;
+            _Author=memento.Author;
+            _isbn=memento.Isbn;
+            _lastEdaited=memento.LastEdaited;
+        }
+        public void ShowBook()
+        {
+            Console.WriteLine("{0},{1},{2} edited : {3}",İsbn,Title,Author,_lastEdaited);
+        }
+    }
+    class Memento
+    {
+        public string Isbn { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public DateTime LastEdaited { get; set; }
+        
+        public Memento(string isbn,string title,string author,DateTime lastEdited)
+        {
+            Isbn = isbn;
+            Title = title;
+            Author = author;
+            LastEdaited = lastEdited;
+        }
+    }
+    class CareTaker
+    {
+        public Memento Memento { get; set; }
+    }
+}
---
+++ Kod ders/TemplateMethod/Program.cs
@@ -0,0 +1,90 @@
+﻿// See https://aka.ms/new-console-template for more information
+using static System.Formats.Asn1.AsnWriter;
+
+namespace TemplateMethod
+{
+    class Template
+    {
+        static void Main(string[] args)
+        {
+            ScoringAlgoritma algoritma;
+            Console.WriteLine("Mans");
+            algoritma = new MensScoringAlgoritma();
+            Console.WriteLine(algoritma.GenerateScore(10,new TimeSpan(0,2,34)));
+
+            Console.WriteLine("Womens");
+            algoritma = new WomensScoringAlgoritma();
+            Console.WriteLine(algoritma.GenerateScore(10, new TimeSpan(0, 2, 34)));
+
+            Console.WriteLine("Childrens");
+            algoritma = new ChildrensScoringAlgoritma();
+            Console.WriteLine(algoritma.GenerateScore(10, new TimeSpan(0, 2, 34)));
+
+            Console.ReadLine();
+        }
+    }
+    abstract class ScoringAlgoritma
+    {
+        public int GenerateScore(int hits, TimeSpan time)
+        {
+            int score = CalculateBaseScore(hits);
+            int reduction = CalculateReduction(time);
+            return CalculateOverAllScore(score,reduction);
+        }
+
+        public abstract int CalculateBaseScore(int hits);
+        public abstract int CalculateOverAllScore(int score,int reduction);
+        public abstract int CalculateReduction(TimeSpan time);
+    }
+    class MensScoringAlgoritma : ScoringAlgoritma
+    {
+        public override int CalculateBaseScore(int hits)
+        {
+            return hits * 100;
+        }
+
+        public override int CalculateOverAllScore(int score, int reduction)
+        {
+            return score - reduction;
+        }
+
+        public override int CalculateReduction(TimeSpan time)
+        {
+            return (int)time.TotalSeconds / 3;
+        }
+    }
+    class WomensScoringAlgoritma : ScoringAlgoritma
+    {
+        public override int CalculateBaseScore(int hits)
+        {
+            return hits * 100;
+        }
+
+        public override int CalculateOverAllScore(int score, int reduction)
+        {
+            return score - reduction;
+        }
+
+        public override int CalculateReduction(TimeSpan time)
+        {
+            return (int)time.TotalSeconds / 2;
+        }
+    }
+    class ChildrensScoringAlgoritma : ScoringAlgoritma
+    {
+        public override int CalculateBaseScore(int hits)
+        {
+            return hits * 80;
+        }
+
+        public override int CalculateOverAllScore(int score, int reduction)
+        {
+            return score - reduction;
+        }
+
+        public override int CalculateReduction(TimeSpan time)
+        {
+            return (int)time.TotalSeconds / 5;
+        }
+    }
+}
