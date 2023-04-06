ea2cbb25ea1523f6621c11b856cc532acdca5fe1
Yusuf <yusuf_zeki1234@hotmail.com>
2023-04-06 17:05:10 +0300

    New Work Added

---
+++ Kod ders/MeditatorDesign/Program.cs
@@ -0,0 +1,100 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace MeditatorDesign
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mediator mediator = new Mediator();
+            Teacher Yusuf = new Teacher(mediator);
+            Yusuf.name = " Yusuf ";
+            mediator.Teacher = Yusuf;
+
+            Student eda = new Student(mediator);
+            eda.name = "Eda";
+
+            Student pınar = new Student(mediator);
+            pınar.name = "Pınar";
+
+            mediator.Student = new List<Student> { eda, pınar };
+
+            Yusuf.SendNewImage("Slide1.jpg");
+            Yusuf.RecieveQuestions("is it True",eda);
+
+            Console.ReadLine();
+
+        }
+    }
+    abstract class CourseMember
+    {
+        protected Mediator Mediator;
+        protected CourseMember(Mediator mediator)
+        {
+            Mediator = mediator;
+        }
+    }
+    class Teacher : CourseMember
+    {
+        public string name { get; set; }
+        public Teacher(Mediator mediator) : base(mediator)
+        {
+
+        }
+
+        public void RecieveQuestions(string questions, Student student)
+        {
+            Console.WriteLine("Teacher recieve a questions from {0},{1}",student.name,questions);
+        }
+        public void SendNewImage(string url)
+        {
+            Console.WriteLine("Teacher changed slide : {0}",url);
+            Mediator.UpdateImage(url);
+        }
+        public void Answer(string answer,Student student)
+        {
+            Console.WriteLine("Teacher answered questions {0},{1}",student.name,answer);
+        }
+    }
+    class Student : CourseMember
+    {
+        public Student(Mediator mediator) : base(mediator)
+        {
+
+        }
+
+        public string name { get; set; }
+
+        public void RecieveImage(string url)
+        {
+            Console.WriteLine("{1} recieved image {0}",url,name);   
+        }
+
+        public void RecieveAnswer(string answer)
+        {
+            Console.WriteLine("Student recieved Answer {0}", answer);
+        }
+    }
+    class Mediator
+    {
+        public Teacher Teacher { get; set; }
+        public List<Student> Student { get; set; }
+
+        public void UpdateImage(string url)
+        {
+            foreach (var students in Student)
+            {
+                students.RecieveImage(url);
+            }
+        }
+        public void SendQuestion(string questions,Student student)
+        {
+            Teacher.RecieveQuestions(questions, student);
+        }
+        public void SendAnswer(string answer,Student student)
+        {
+            student.RecieveAnswer(answer);
+        }
+    }
+}
---
+++ Kod ders/StateDesign/StateDesign/Program.cs
@@ -0,0 +1,69 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace StateDesign
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Context context = new Context();
+            ModifiedState modified = new ModifiedState();
+            modified.DoAction(context);
+
+            Console.WriteLine(context.GetState().ToString());
+
+            Console.ReadLine();
+        }
+    }
+    interface IState
+    {
+        void DoAction(Context context);
+    }
+    class ModifiedState:IState
+    {
+        public void DoAction(Context context)
+        {
+            Console.WriteLine("State : Modified");
+            context.SetState(this);
+        }
+        public override string ToString()
+        {
+            return "Modified";
+        }
+    }
+    class DeletedState : IState
+    {
+        public void DoAction(Context context)
+        {
+            Console.WriteLine("State : Deleted");
+            context.SetState(this);
+        }
+        public override string ToString()
+        {
+            return "Deleted";
+        }
+    }
+    class AddedState : IState
+    {
+        public void DoAction(Context context)
+        {
+            Console.WriteLine("State : Added");
+            context.SetState(this);
+        }
+        public override string ToString()
+        {
+            return "Added";
+        }
+    }
+    internal class Context
+    {
+        private IState _state;
+        public void SetState(IState state)
+        {
+            _state=state;
+        }
+        public IState GetState()
+        {
+            return _state;
+        }
+    }
+}
