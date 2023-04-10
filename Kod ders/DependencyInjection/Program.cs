232d11ec05d78e45692b1262179a2f8a3a06163d
Yusuf <yusuf_zeki1234@hotmail.com>
2023-04-10 17:38:09 +0300

    New Work Added

---
+++ Kod ders/DependencyInjection/Program.cs
@@ -0,0 +1,51 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Ninject;
+
+namespace DependencyInjection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IKernel kernel =new StandardKernel();
+            kernel.Bind<IProductDal>().To<EFProductDal>().InSingletonScope();
+
+            ProductManager productManager = new ProductManager(new EFProductDal());
+            productManager.Save();
+            Console.ReadLine();
+        }
+    }
+    interface IProductDal
+    {
+        void Save();
+    }
+    class EFProductDal : IProductDal
+    {
+        public void Save()
+        {
+            Console.WriteLine("Saved with EF");
+        }
+    }
+    class NHProductDal : IProductDal
+    {
+        public void Save()
+        {
+            Console.WriteLine("Saved with NH");
+        }
+    }
+    class ProductManager
+    {
+        private IProductDal _productDal;
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal= productDal;
+        }
+        public void Save()
+        {
+
+            //Business Code
+            
+            _productDal.Save();
+        }
+    }
+}
