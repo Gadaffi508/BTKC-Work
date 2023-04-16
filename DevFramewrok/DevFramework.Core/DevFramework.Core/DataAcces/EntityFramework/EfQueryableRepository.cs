d6515f5cbe0e7e5038c4a6436c6eb52229bc0ccd
Yusuf <yusuf_zeki1234@hotmail.com>
2023-04-16 17:47:36 +0300

    New Work Added

---
+++ DevFramewrok/DevFramework.Core/DevFramework.Core/DataAcces/EntityFramework/EfQueryableRepository.cs
@@ -0,0 +1,26 @@
+﻿using DevFramework.Core.DataAcces.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFramework.Core.DataAcces.EntityFramework
+{
+    public class EfQueryableRepository<T>: IQueryableRepository<T> where T:class ,IEntity,new()
+    {
+        private DbContext _dbContext;
+        private IDbSet<T> _entities;
+        public EfQueryableRepository(DbContext context)
+        {
+            _dbContext = context;
+        }
+        public IQueryable<T> Table => this._entities;
+
+        protected virtual IDbSet<T> Entities
+        {
+            get { return _entities ?? (_entities = _dbContext.Set<T>()); }
+        }
+    }
+}
